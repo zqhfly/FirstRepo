99e0e4a89a52dbc6b7c32dcc3428d968a9919844
zqhfly <1762029177@qq.com>
2014-06-23 09:41:56 +0800

    Update Branch2.java

--- src/com/wantdo/Branch2.java
+++ src/com/wantdo/Branch2.java
@@ -8,7 +8,6 @@ public class Branch2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
-		System.out.println("hello world!");
 		
 	}
 
