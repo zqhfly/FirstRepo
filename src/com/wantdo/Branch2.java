10377b9e2bf64f95e0cf7a23a96c85c711aa25ee
Administrator <Administrator@PC-20140528TYVZ>
2014-06-23 09:36:05 +0800

    Signed-off-by: Administrator <Administrator@PC-20140528TYVZ>

--- src/com/wantdo/Branch2.java
+++ src/com/wantdo/Branch2.java
@@ -8,6 +8,7 @@ public class Branch2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
+		System.out.println("hello world!");
 		
 	}
 
